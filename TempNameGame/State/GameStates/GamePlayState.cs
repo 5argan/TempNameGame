8c21dba1c6a4d8f03a75489e0840afaf274bf522
5argan <tizian.hoffmann@gmail.com>
2017-02-23 17:06:55 +0100

    Added gameplay state

---
+++ TempNameGame/State/GameStates/GamePlayState.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using TempNameGame.TileEngine;
+
+namespace TempNameGame.State.GameStates
+{
+    public interface IGamePlayState
+    {
+        
+    }
+    public class GamePlayState : GameStateBase, IGamePlayState
+    {
+        Engine engine = new Engine(Game1.ScreenRectangle, 64, 64);
+        public GamePlayState(Game game) : base(game)
+        {
+            game.Services.AddService(typeof(IGamePlayState), this);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
