368fd0eeddf632ee26995e2c965061c166995880
5argan <tizian.hoffmann@gmail.com>
2017-02-23 22:19:10 +0100

    Added player class

---
+++ TempNameGame/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TempNameGame.TileEngine;
+
+namespace TempNameGame
+{
+    public class Player : DrawableGameComponent
+    {
+        private Game1 _game;
+        private string _name;
+        private bool _gender;
+        private string _mapName;
+        private Point _tile;
+        private AnimatedSprite _sprite;
+        private Texture2D _texture;
+        private float Speed { get; set; } = 180f;
+        private Vector2 _position;
+
+        public Vector2 Position
+        {
+            get { return _sprite.Position; }
+            set { _sprite.Position = value; }
+        }
+
+        public AnimatedSprite Sprite => _sprite;
+
+        private Player(Game game) : base(game)
+        {
+        }
+
+        public Player(Game game, string name, bool gender, Texture2D texture) : base(game)
+        {
+            _game = (Game1) game;
+            _name = name;
+            _gender = gender;
+            _texture = texture;
+            _sprite = new AnimatedSprite(texture, _game.playerAnimations)
+            {
+                CurrentAnimation = AnimationKey.WalkDown
+            };
+        }
+
+        public void SavePlayer()
+        {
+            
+        }
+
+        public static Player Load(Game game)
+        {
+            return new Player(game);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            _sprite.Draw(gameTime, _game.SpriteBatch);
+        }
+    }
+}
