d8f07611e8d13d53cb7f6ee1788ffcf36b23afcf
5argan <tizian.hoffmann@gmail.com>
2017-02-24 14:02:20 +0100

    Added character manager class

---
+++ TempNameGame/CharacterComponents/CharacterManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TempNameGame.CharacterComponents
+{
+    public class CharacterManager
+    {
+        private readonly Dictionary<string, ICharacter> _characters = new Dictionary<string, ICharacter>();
+        public static CharacterManager Instance { get; } = new CharacterManager();
+
+        private CharacterManager()
+        {
+            
+        }
+
+        public ICharacter GetCharacter(string name)
+        {
+            return _characters.ContainsKey(name) ? _characters[name] : null;
+        }
+
+        public void AddCharacter(string name, ICharacter character)
+        {
+            if (!_characters.ContainsKey(name))
+            {
+                _characters.Add(name, character);
+            }
+        }
+    }
+}
