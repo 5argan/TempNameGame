30e5a30aaf79617eb3b27aa2aea8b00416bb2d2d
5argan <tizian.hoffmann@gmail.com>
2017-02-26 12:55:49 +0100

    Added scenes for dialogue

---
+++ TempNameGame/ConversationComponents/SceneOption.cs
@@ -0,0 +1,26 @@
+﻿namespace TempNameGame.ConversationComponents
+{
+    public enum ActionType
+    {
+        Talk, End, Change, Quest, Buy, Sell, GiveItems, GiveKey
+    }
+
+    public class SceneAction
+    {
+        public ActionType Action;
+        public string Parameter;
+    }
+    public class SceneOption
+    {
+        public string OptionText { get; set; }
+        public string OptionScene { get; set; }
+        public SceneAction OptionAction { get; set; }
+
+        public SceneOption(string text, string scene, SceneAction action)
+        {
+            OptionText = text;
+            OptionScene = scene;
+            OptionAction = action;
+        }
+    }
+}
