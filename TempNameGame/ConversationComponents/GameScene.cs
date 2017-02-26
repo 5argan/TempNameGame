f59d12d3db7f60c1e177895fbed1cf0f1c990cab
5argan <tizian.hoffmann@gmail.com>
2017-02-26 13:01:58 +0100

    Started GameScene class for dialogues

---
+++ TempNameGame/ConversationComponents/GameScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TempNameGame.ConversationComponents
+{
+    public class GameScene
+    {
+        protected Game _game;
+        protected string _text;
+        public int selectedIndex { get; }
+
+        public string Text
+        {
+            get { return _text;}
+            set { _text = value; }
+        }
+
+        public static Texture2D Selected { get; }
+        public List<SceneOption> Options { get; set; }
+
+        public bool IsMouseOver { get; }
+
+        [ContentSerializerIgnore]
+        public Color NormalColor { get; set; }
+
+        [ContentSerializerIgnore]
+        public Color HighlightColor { get; set; }
+
+        public Vector2 MenuPosition { get; }
+
+        [ContentSerializerIgnore]
+        public SceneAction OptionAction => Options[selectedIndex].OptionAction;
+
+        public string OptionScene => Options[selectedIndex].OptionScene;
+
+        public string OptionText => Options[selectedIndex].OptionText;
+    }
+}
