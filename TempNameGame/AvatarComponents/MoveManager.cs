74d0563642407dae8963a43dcec9e7311f2406ed
5argan <tizian.hoffmann@gmail.com>
2017-02-26 16:42:49 +0100

    Added move manager

---
+++ TempNameGame/AvatarComponents/MoveManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TempNameGame.AvatarComponents
+{
+    public class MoveManager
+    {
+        private static Dictionary<string, IMove> allMoves = new Dictionary<string, IMove>();
+        public static Random Random { get; } = new Random();
+
+        public static void FillMoves()
+        {
+
+        }
+
+        public static IMove GetMove(string name) => 
+            allMoves.ContainsKey(name) ? (IMove) allMoves[name].Clone() : null;
+
+        public static void AddMove(IMove move)
+        {
+            if (!allMoves.ContainsKey(move.Name))
+                allMoves.Add(move.Name, move);
+        }
+    }
+}
